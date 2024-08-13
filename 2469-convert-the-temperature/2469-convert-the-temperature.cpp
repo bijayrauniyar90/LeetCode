dc49f266a8bc912eaaf1b359ac9f113cdb5f2b85
Bijay Prasad Shah Rauniyar <105978662+bijayrauniyar90@users.noreply.github.com>
2024-08-14 00:51:44 +0530

    Time: 3 ms (44.79%), Space: 7.8 MB (37.02%) - LeetHub

--- 2469-convert-the-temperature/2469-convert-the-temperature.cpp
+++ 2469-convert-the-temperature/2469-convert-the-temperature.cpp
@@ -1,11 +1,18 @@
+// class Solution {
+// public:
+//     vector<double> convertTemperature(double celsius) {
+//         double kelvin = celsius + 273.15;
+//         double fahrenheit = celsius * 1.80 + 32.00;
+//         vector<double> v;
+//         v.push_back(kelvin); 
+//         v.push_back(fahrenheit);
+//         return v;
+//     }
+// };
+
 class Solution {
 public:
     vector<double> convertTemperature(double celsius) {
-        double kelvin = celsius + 273.15;
-        double fahrenheit = celsius * 1.80 + 32.00;
-        vector<double> v;
-        v.push_back(kelvin); 
-        v.push_back(fahrenheit);
-        return v;
+        return {celsius + 273.15, celsius * 1.8 + 32.0};
     }
 };
